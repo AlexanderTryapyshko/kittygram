c0895751d62b1bbf20ba53ff70a8d67003e88f4c
Alexander Tryapyshko <stoolik@gmail.com>
2024-12-05 19:11:39 +0300

    after second review

--- backend/kittygram_backend/utils.py
+++ backend/kittygram_backend/utils.py
@@ -1,10 +1,11 @@
 """Утилиты для определения хостов и состояния дебага."""
-
 import os
+
 
 def get_debug():
     return os.getenv('DEBUG', 'True').lower() == 'true'
 
+
 def get_allowed_hosts():
-    allowed_hosts  = os.getenv('ALLOWED_HOSTS', 'localhost,127.0.0.1')
+    allowed_hosts = os.getenv('ALLOWED_HOSTS', 'localhost,127.0.0.1')
     return [host.strip() for host in allowed_hosts.split(',')]
